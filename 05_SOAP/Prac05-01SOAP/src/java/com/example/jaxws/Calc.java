520094d9df1cc1aeefd1db211c277146a8847124
Jesús Ángel Samitier <eckelon@gmail.com>
2017-02-22 18:57:07 +0100

    first commit!

---
+++ 05_SOAP/Prac05-01SOAP/src/java/com/example/jaxws/Calc.java
@@ -0,0 +1,14 @@
+package com.example.jaxws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Calc {
+  
+  // Annotate this method
+  public String squared(String number){
+    // Calculate and return the result
+    return "";
+  }  
+}
